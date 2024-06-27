b979537d39978f149a7bd30720c5ec4c8f9fc4d6
Admin <2762713521@qq.com>
2024-06-28 01:29:32 +0800

    update widgets

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	app.Run("godoc", func(w *unison.Window) {
-		// godoc.New("D:\\workspace\\workspace\\app").Layout(w.Content())
-		godoc.New("D:\\workspace\\workspace\\branch\\golibrary").Layout(w.Content())
+		// godoc.New("D:\\workspace\\workspace\\app").Layout()
+		w.Content().AddChild(godoc.New("D:\\workspace\\workspace\\branch\\golibrary").Layout())
 	})
 }
